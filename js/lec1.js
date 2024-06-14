a94d33326781601d9a608a057fd9d1dfe7a3a6f8
Arham Malik <arhammalik900@yahoo.com>
2024-06-14 09:58:24 +0500

    Add files via upload

---
+++ js/lec1.js
@@ -0,0 +1,27 @@
+// confirm("Hellow");
+// alert('hellow')
+// prompt('hellow')
+
+
+console.log("my name is arham")
+
+
+console.log('15 / 2 = ' + (15 / 2));
+console.log('15 * 2 = ' + (15 * 2));
+console.log('15 + 2 = ' + (15 + 2));
+console.log('15 - 2 = ' + (15 - 2));
+
+// let a = 3;
+let b = "Hello ";
+var c = 3;
+const d = 20.5;
+
+console.log(b + d)
+{
+    var a = 5
+    const d = 2
+    console.log(a * d)
+}
+
+
+console.log(a * d)
